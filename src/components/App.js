f279b5ebbdae768a4aeeb8d534d95d0e014aeabe
Chris Agold <christophersagold@gmail.com>
2021-06-07 16:19:24 -0500

    added note component

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import React from "react";
 import Header from './Header.jsx';
 import Footer from './Footer.jsx';
+import Note from './Note.jsx';
 
 function App() {
     return (
         <div>
             <Header/>
+            <Note/>
             <Footer/>
         </div>
     );
