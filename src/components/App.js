89abcf64ea2bca58d096f1a2ed9692c8a03020fe
Chris Agold <christophersagold@gmail.com>
2021-06-07 15:04:58 -0500

    added Footer Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import React from "react";
 import Header from './Header.jsx';
+import Footer from './Footer.jsx';
 
 function App() {
     return (
         <div>
             <Header/>
+            <Footer/>
         </div>
     );
 }
